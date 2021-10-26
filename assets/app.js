15afce7fad59c9fc0c2737ba5877ba26c6906fd2
ZK9XW3 <peteralexandremusic@gmail.com>
2021-10-26 14:10:03 +0200

    feat(modules): added bootswatch

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,9 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+// import bootswatch
+// import "bootswatch/dist/minty/bootstrap.min.css";
+
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
