1f615e43af223c4064b1a6e51984dca783cafa37
ZK9XW3 <peteralexandremusic@gmail.com>
2021-11-01 10:55:11 +0100

    feat():postsList made dynamic

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PostsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,21 +12,24 @@ class PostController extends AbstractController
     /**
      * @Route("/posts", name="posts_list")
      */
-    public function postsList(): Response
+    public function postsList(PostsRepository $postsRepository): Response
     {
+        $postsList = $postsRepository->findAll();
+
         return $this->render('post/posts-list.html.twig', [
-            'controller_name' => 'PostController',
+            'postsList' => $postsList,
         ]);
     }
 
     /**
-     * @Route("/post/id", name="post")
+     * @Route("/post/{id}", name="post", methods={"GET"}, requirements={"id": "\d+"})
      */
-    public function post(): Response
+    public function post(PostsRepository $postsRepository, int $id): Response
     {
+        $post = $postsRepository->find($id);
+
         return $this->render('post/post.html.twig', [
-            'controller_name' => 'PostController',
-            // 'urlController' => $this->container->get('router')->getContext()->getBaseUrl(),
+            'posts' => $post,
         ]);
     }
 
