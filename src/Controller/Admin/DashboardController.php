3f9e37cfd341ec73e2d1428af36ed1954bbf9e3f
ZK9XW3 <peteralexandremusic@gmail.com>
2021-11-02 16:10:06 +0100

    feat(categoriesCrudController): made categories crud ea

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Categories;
 use App\Entity\Posts;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
@@ -33,6 +34,7 @@ class DashboardController extends AbstractDashboardController
             MenuItem::section('Administrate'),
             MenuItem::linkToCrud('Posts', 'fas fa-pen-square', Posts::class),
             MenuItem::linkToCrud('User', 'fas fa-user', User::class),
+            MenuItem::linkToCrud('Categories', 'fas fa-newspaper', Categories::class),
             MenuItem::section('Navigate'),
             MenuItem::linkToRoute('Back to main site', 'fa fa-home', 'main'),
             MenuItem::linkToLogout('logout', 'fa fa-sign-out'),
--- src/Controller/Admin/PostsCrudController.php
+++ src/Controller/Admin/PostsCrudController.php
@@ -4,13 +4,13 @@ namespace App\Controller\Admin;
 
 use App\Entity\Posts;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use Vich\UploaderBundle\Form\Type\VichImageType;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class PostsCrudController extends AbstractCrudController
 {
@@ -27,6 +27,7 @@ class PostsCrudController extends AbstractCrudController
             TextField::new('title'),
             TextareaField::new('text'),
             TextField::new('slug')->hideOnForm(), 
+            // Field::new('category'),
             DateField::new('created_at')->hideOnForm(),
             TextField::new('imageFile')->setFormType(VichImageType::class)->onlyWhenCreating(),
             ImageField::new('image')->setBasePath('/uploads/posts')->onlyOnIndex(),
