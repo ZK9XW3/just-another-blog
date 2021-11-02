0917c28efeab6b21d6b469d4e4048daba96c4683
ZK9XW3 <peteralexandremusic@gmail.com>
2021-11-02 11:42:42 +0100

    feat(main):About-section and links dynamic

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\PostsRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,16 +13,22 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="main")
      */
-    public function index(PostsRepository $postsRepository): Response
+    public function index(PostsRepository $postsRepository, UserRepository $userRepository): Response
     {
+        // Get 3 last posts from db
         $postsToDisplay = $postsRepository->findBy(
             [],
             ['created_at' => 'DESC'],
             3,
         );
 
+        // Get Admin user's data by email
+        $user = $userRepository->findOneBy(['email' => 'john@doe.com']);
+
+
         return $this->render('main/index.html.twig', [
             'postsToDisplay' => $postsToDisplay,
+            'user' => $user,
         ]);
     }
 
