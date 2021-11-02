0511d2fc5ad6c9ab560a188cc5e29033a32a817c
ZK9XW3 <peteralexandremusic@gmail.com>
2021-11-02 16:25:33 +0100

    feat(categoriesCrduController): added categories to posts

--- src/Controller/Admin/PostsCrudController.php
+++ src/Controller/Admin/PostsCrudController.php
@@ -9,8 +9,11 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 
 class PostsCrudController extends AbstractCrudController
 {
@@ -27,7 +30,7 @@ class PostsCrudController extends AbstractCrudController
             TextField::new('title'),
             TextareaField::new('text'),
             TextField::new('slug')->hideOnForm(), 
-            // Field::new('category'),
+            AssociationField::new('category'),
             DateField::new('created_at')->hideOnForm(),
             TextField::new('imageFile')->setFormType(VichImageType::class)->onlyWhenCreating(),
             ImageField::new('image')->setBasePath('/uploads/posts')->onlyOnIndex(),
