6ea5a2b1d0b4f58172533540009f2cc39c7e5991
ZK9XW3 <peteralexandremusic@gmail.com>
2021-11-03 23:31:46 +0100

    feat(vich.yml/posts):namer off and posts datetime update

--- src/Entity/Posts.php
+++ src/Entity/Posts.php
@@ -138,7 +138,7 @@ class Posts
         if (null !== $imageFile) {
             // It is required that at least one field changes if you are using doctrine
             // otherwise the event listeners won't be called and the file is lost
-            $this->updatedAt = new \DateTimeImmutable();
+            $this->updatedAt = new \DateTime('now');
         }
     }
 
